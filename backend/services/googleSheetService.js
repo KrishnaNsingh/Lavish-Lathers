8a08745aaed2ec1afdf443bafde172a5401e33cb
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-06-09 20:24:25 +0530

    refactor: Remove deprecated crypto package and update Google Sheets service authentication method

--- backend/services/googleSheetService.js
+++ backend/services/googleSheetService.js
@@ -1,7 +1,15 @@
 const { google } = require("googleapis");
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: "./config/google-service-account.json",
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(
+      /\\n/g,
+      "\n"
+    ),
+  },
+
   scopes: [
     "https://www.googleapis.com/auth/spreadsheets",
   ],
